466be592f817cebcaa8f16277c62db04e71c5425
boyfromhell <boyfromhell86@gmail.com>
2023-07-30 23:52:54 +0300

    Update settings.blade.php

--- resources/views/pages/settings.blade.php
+++ resources/views/pages/settings.blade.php
@@ -6,9 +6,6 @@
         <x-filament-panels::form.actions
             :actions="$this->getFormActions()"
             />
-{{--        <x-tables::button type="submit" class="mt-2">--}}
-{{--            @lang('Save')--}}
-{{--        </x-tables::button>--}}
 
     </x-filament-panels::form>
 </x-filament-panels::page>
